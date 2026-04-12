586358df7479db5b3d4b71dfbb469872326d9ef4
Dineshkumar524 <dineshkumarpurve1@gmail.com>
2026-04-12 15:58:52 +0530

    Update 675_02_A2_01.c

--- 675_02_A2_01.c
+++ 675_02_A2_01.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+int linear_search(int *arr, int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
 
-int main()
-{
-    int n, i, key, pos = -1;
+int main() {
+    int n, key, index;
     int *arr;
 
-    printf("Enter number of elements: ");
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    arr = (int *)malloc(n * sizeof(int));
-    if(arr == NULL)
-    {
-        printf("Memory allocation failed\n");
+    arr = (int*)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
         return 1;
     }
 
-    printf("Enter elements:\n");
-    for(i = 0; i < n; i++)
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
-
-    printf("Enter key to search: ");
-    scanf("%d", &key);
-
-    for(i = 0; i < n; i++)
-    {
-        if(arr[i] == key)
-        {
-            pos = i;
-            break;
-        }
     }
 
-    if(pos != -1)
-        printf("Element found at position %d\n", pos + 1);
-    else
-        printf("Element not found\n");
+    printf("Enter the key element to find: ");
+    scanf("%d", &key);
+
+    index = linear_search(arr, n, key);
+
+    if (index != -1) {
+        printf("Element %d found at index %d.\n", key, index);
+    } else {
+        printf("Element %d not found in the array.\n", key);
+    }
 
     free(arr);
 
